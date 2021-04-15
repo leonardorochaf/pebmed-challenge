fea66cb23c97791b99e98b6faf8b45b039fdad0e
leonardorochaf <leonardo.rocha0803@gmail.com>
2021-04-15 09:55:11 -0300

    typeorm configuration

---
+++ ormconfig.js
@@ -0,0 +1,37 @@
+const { SnakeNamingStrategy } = require('typeorm-naming-strategies')
+
+module.exports = [
+  {
+    name: 'dev',
+    type: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'root',
+    database: 'pebmed',
+    logging: true,
+    namingStrategy: new SnakeNamingStrategy(),
+    entities: [
+      './src/models/*.ts'
+    ],
+    migrations: [
+      './src/database/migrations/*.ts'
+    ]
+  },
+  {
+    name: 'test',
+    type: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'root',
+    database: 'pebmed-test',
+    dropSchema: true,
+    migrationsRun: true,
+    namingStrategy: new SnakeNamingStrategy(),
+    entities: [
+      './src/models/*.ts'
+    ],
+    migrations: [
+      './src/database/migrations/*.ts'
+    ]
+  }
+]
