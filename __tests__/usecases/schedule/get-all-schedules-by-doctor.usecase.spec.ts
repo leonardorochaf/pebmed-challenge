22c3a687340f615fe1ba77f458c6741e39001d1e
leonardorochaf <leonardo.rocha0803@gmail.com>
2021-04-18 18:55:08 -0300

    ensure GetAllSchedulesByDoctorUsecase call GetAllSchedulesByDoctorRepository with correct value

--- __tests__/usecases/schedule/get-all-schedules-by-doctor.usecase.spec.ts
+++ __tests__/usecases/schedule/get-all-schedules-by-doctor.usecase.spec.ts
@@ -1,10 +1,35 @@
 import faker from 'faker'
+
+import { Schedule } from '../../../src/models/Schedule'
+import { IGetAllSchedulesByDoctorRepository } from '../../../src/repositories/schedule/interfaces/get-all-schedules-by-doctor.repository.interface'
 import { IDecodeToken } from '../../../src/services/jwt/interfaces/decode-token.interface'
 import { GetAllSchedulesByDoctorUsecase } from '../../../src/usecases/schedule/get-all-schedules-by-doctor.usecase'
+import { Gender } from '../../../src/utils/gender-enum'
 
 const mockRequestToken = faker.datatype.uuid()
 
 const mockDecodeTokenResponse = faker.datatype.uuid()
+
+const mockResponse = [{
+  id: faker.datatype.uuid(),
+  time: faker.date.future(),
+  createdAt: faker.date.past(),
+  updatedAt: faker.date.past(),
+  patient: {
+    id: faker.datatype.uuid(),
+    name: faker.name.findName(),
+    phone: faker.phone.phoneNumber(),
+    email: faker.internet.email(),
+    birthday: faker.date.past(),
+    gender: Gender.MASCULINO,
+    height: faker.datatype.float({ min: 0, max: 2.5 }),
+    weight: faker.datatype.float({ min: 0, max: 100 }),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.past(),
+    deletedAt: null
+  },
+  doctor: null
+}]
 
 class DecodeTokenStub implements IDecodeToken {
   async decode (token: string): Promise<string> {
@@ -12,17 +37,26 @@ class DecodeTokenStub implements IDecodeToken {
   }
 }
 
+class GetAllSchedulesByDoctorRepositoryStub implements IGetAllSchedulesByDoctorRepository {
+  async getAllByDoctor (doctorId: string): Promise<Schedule[]> {
+    return mockResponse
+  }
+}
+
 type SutTypes = {
   sut: GetAllSchedulesByDoctorUsecase
   decodeTokenStub: DecodeTokenStub
+  getAllSchedulesByDoctorRepositoryStub: GetAllSchedulesByDoctorRepositoryStub
 }
 
 const sutFactory = (): SutTypes => {
+  const getAllSchedulesByDoctorRepositoryStub = new GetAllSchedulesByDoctorRepositoryStub()
   const decodeTokenStub = new DecodeTokenStub()
-  const sut = new GetAllSchedulesByDoctorUsecase(decodeTokenStub)
+  const sut = new GetAllSchedulesByDoctorUsecase(decodeTokenStub, getAllSchedulesByDoctorRepositoryStub)
   return {
     sut,
-    decodeTokenStub
+    decodeTokenStub,
+    getAllSchedulesByDoctorRepositoryStub
   }
 }
 
@@ -41,5 +75,12 @@ describe('Get All Schedules By Doctor Usecase', () => {
     })
     const promise = sut.execute(mockDecodeTokenResponse)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call GetAllSchedulesByDoctorRepository with correct id', async () => {
+    const { sut, getAllSchedulesByDoctorRepositoryStub } = sutFactory()
+    const getAllByDoctorSpy = jest.spyOn(getAllSchedulesByDoctorRepositoryStub, 'getAllByDoctor')
+    await sut.execute(mockRequestToken)
+    expect(getAllByDoctorSpy).toHaveBeenCalledWith(mockDecodeTokenResponse)
   })
 })
--- src/usecases/schedule/get-all-schedules-by-doctor.usecase.ts
+++ src/usecases/schedule/get-all-schedules-by-doctor.usecase.ts
@@ -1,15 +1,18 @@
 import { IDecodeToken } from '../../services/jwt/interfaces/decode-token.interface'
 import { DefaultScheduleResponse } from '../../dtos/schedule/default-schedule-response'
 import { IGetAllSchedulesByDoctorUsecase } from './interfaces/get-all-schedules-by-doctor.usecase.interface'
+import { IGetAllSchedulesByDoctorRepository } from '../../repositories/schedule/interfaces/get-all-schedules-by-doctor.repository.interface'
 
 export class GetAllSchedulesByDoctorUsecase implements IGetAllSchedulesByDoctorUsecase {
   constructor (
-    private readonly decodeToken: IDecodeToken
+    private readonly decodeToken: IDecodeToken,
+    private readonly getAllSchedulesByDoctorRepository: IGetAllSchedulesByDoctorRepository
   ) { }
 
   async execute (token: string): Promise<DefaultScheduleResponse[]> {
-    await this.decodeToken.decode(token)
+    const doctorId = await this.decodeToken.decode(token)
 
+    await this.getAllSchedulesByDoctorRepository.getAllByDoctor(doctorId)
     return Promise.resolve(null)
   }
 }
