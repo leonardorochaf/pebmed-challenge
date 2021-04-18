194b9bd21725f1e2b05a769f9cf5a1ff98ab0fe8
leonardorochaf <leonardo.rocha0803@gmail.com>
2021-04-17 23:31:52 -0300

    ensure DeletePatientUsecase throw if DeletePatientRepository throws

--- __tests__/usecases/patient/delete-patient.usecase.spec.ts
+++ __tests__/usecases/patient/delete-patient.usecase.spec.ts
@@ -56,4 +56,13 @@ describe('Delete Patient Usecase', () => {
     const promise = sut.execute(mockRequestId)
     await expect(promise).rejects.toThrow(PatientNotFoundError)
   })
+
+  test('Should throw if GetPatientByIdRepository throws', async () => {
+    const { sut, getPatientByIdRepositoryStub } = sutFactory()
+    jest.spyOn(getPatientByIdRepositoryStub, 'getById').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.execute(mockRequestId)
+    await expect(promise).rejects.toThrow()
+  })
 })
